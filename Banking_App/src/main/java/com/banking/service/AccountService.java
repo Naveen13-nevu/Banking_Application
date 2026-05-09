f31907f7d26849aff02ff5ffdfc1883f50c31f35
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-05-09 17:50:28 +0530

    update

--- Banking_App/src/main/java/com/banking/service/AccountService.java
+++ Banking_App/src/main/java/com/banking/service/AccountService.java
@@ -2,9 +2,14 @@ package com.banking.service;
 
 import com.banking.dto.AccountDto;
 
+import java.util.List;
+
 public interface AccountService {
 
     AccountDto createAccount(AccountDto account);
     AccountDto getAccountById(Long id);
     AccountDto deposit(Long id, double amount);
+    AccountDto withdraw(Long id, double amount);
+    List<AccountDto> getAllaccounts();
+    void deleteAccount(Long id);
 }
--- Banking_App/src/main/java/com/banking/service/implementation/AccountServiceImpl.java
+++ Banking_App/src/main/java/com/banking/service/implementation/AccountServiceImpl.java
@@ -7,6 +7,9 @@ import com.banking.repository.AccountRepository;
 import com.banking.service.AccountService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AccountServiceImpl implements AccountService {
 
@@ -35,6 +38,35 @@ public class AccountServiceImpl implements AccountService {
         account.setBalance(total);
        Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public AccountDto withdraw(Long id, double amount) {
+        Account account = accountRepository.findById((id)).orElseThrow(()-> new RuntimeException("unable to find id"+ id));
+
+        if(account.getBalance() < amount){
+            throw new RuntimeException("Insufficient amount");
+
+        }
+        double total = account.getBalance() - amount;
+        account.setBalance(total);
+        Account savedAccount = accountRepository.save(account);
+
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public List<AccountDto> getAllaccounts() {
+        List<Account> accounts = accountRepository.findAll();
+        return   accounts.stream().map((account)->AccountMapper.mapToAccountDto(account))
+                .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public void deleteAccount(Long id) {
+        Account account = accountRepository.findById((id)).orElseThrow(()-> new RuntimeException("unable to find id"+ id));
+        accountRepository.deleteById(id);
     }
 
 
